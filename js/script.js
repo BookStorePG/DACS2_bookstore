f4f79ab05a94f664dc436660c808e7a31b01bf36
Huệ <137085692+Hue030324@users.noreply.github.com>
2023-11-19 16:59:28 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,26 +5,7 @@ document.querySelector('#user-btn').onclick = () =>{
     accountBox.classList.toggle('active');
 }
 
-searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-}
-
-
-// let loginForm = document.querySelector('.login-form-container');
-
-// document.querySelector('#login-btn').onclick = () =>{
-//     loginForm.classList.toggle('active');
-// }
-
-// document.querySelector('#close-login-btn').onclick = () =>{
-//     loginForm.classList.remove('active');
-// }
-
 window.onscroll = () =>{
-
-    searchForm.classList.remove('active');
 
     if(window.scrollY > 80){
         document.querySelector('.header .header-2').classList.add('active');
