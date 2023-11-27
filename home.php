285c3cf245ffdb2289e6a6101283d5208f0dbf34
Huệ <137085692+Hue030324@users.noreply.github.com>
2023-11-27 22:26:29 +0700

    Update home.php

--- home.php
+++ home.php
@@ -3,24 +3,7 @@
 include 'connect.php';
 
 
-if(isset($_POST['add_to_cart'])){
-
-   $product_name = $_POST['product_name'];
-   $product_price = $_POST['product_price'];
-   $product_ori_price = $_POST['product_ori_price'];
-   $product_image = $_POST['product_image'];
-   $product_quantity = $_POST['product_quantity'];
-
-   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
-
-   if(mysqli_num_rows($check_cart_numbers) > 0){
-      $message[] = 'already added to cart!';
-   }else{
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'product added to cart!';
-   }
-
-}
+include 'add_to_cart.php';
 
 ?>
     
@@ -114,7 +97,7 @@ if(isset($_POST['add_to_cart'])){
             <div class="icons">
                 <a href="#" class="fas fa-search"></a>
                 <a href="#" class="fas fa-heart"></a>
-                <a href="#" class="fas fa-eye"></a>
+                <a href="?act=productdetail&id=" class="fas fa-eye"></a>
             </div>
             <div class="image">
                 <img src="img/<?php echo $fetch_products['image']; ?>" alt="">
