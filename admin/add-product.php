b1074201faa14e8b6aab59eef5448526a6521803
HuynhTheAn-24 <149233606+HuynhTheAn-24@users.noreply.github.com>
2023-12-06 15:51:56 +0700

    Update add-product.php

--- admin/add-product.php
+++ admin/add-product.php
@@ -2,39 +2,73 @@
                 <div class="box-heading">
                     <p>Add products</p>
                 </div>
-                <form action="?act=list-product" class="add-product">
+
+
+                <?php
+                    if((isset($_POST['add']))){
+                        $name=$_POST['name']; 
+                        $image=$_FILES['image']['name'];
+                        $image_tmp=$_FILES['image']['tmp_name'];
+                        $price=$_POST['price'];
+                        $original_price=$_POST['original_price'];
+                        $quantity=$_POST['quantity'];
+                        $publication_date=$_POST['publication_date'];
+                        $information=$_POST['information'];
+                        $introduction=$_POST['introduction'];
+                        $path="../img/";
+                    
+                    $add=mysqli_query($conn,"INSERT INTO products (name,information,introduction,image,price,original_price,quantity,publication_date) 
+                                              VALUES ('$name','$information','$introduction','$image','$price','$original_price','$quantity','$publication_date')");
+                    
+                    move_uploaded_file($image_tmp,$path.$image); 
+                    
+                    header("location:?act=list-product");
+
+                    
+                }
+               
+                ?>
+
+
+                <form method="POST"  class="add-product"  enctype="multipart/form-data" >
                     <div class="wrap-box">
                         <table class="table-add">
                             <tr>
                                 <td><label>Name: </label></td>
-                                <td><input type="text" name=" "/></td>
-                            </tr> 
+                                <td><input type="text" name="name"/></td>
+                            </tr>
+                            <tr>
                                 <td><label>image: </label></td>
-                                <td><input type="file" name=" "/></td>
-                            </tr> 
+                                <td><input type="file" name="image"/></td>
+                            </tr>
+                            <tr>
+                                <td><label>quantity: </label></td>
+                                <td><input type="number" name="quantity" min="1"/></td>
+                            </tr>
+                            <tr> 
                                 <td><label>price: </label></td>
-                                <td><input type="text" name=" "/></td>
+                                <td><input type="number" name="price"/></td>
                             </tr>
                             <tr>
                                 <td><label>original price: </label></td>
-                                <td><input type="text" name=" "/></td>
+                                <td><input type="number" name="original_price"/></td>
                             </tr>
                             <tr>
                                 <td><label>publication date: </label></td>
-                                <td><input type="date" name=" "/></td>
+                                <td><input type="date" name="publication_date"/></td>
                             </tr>
                             <tr>
                                 <td><label>information: </label></td>
-                                <td><textarea name="information" cols="" rows="100px" ><?php if(isset($_POST["information"])) echo ($_POST["tomtat"]) ?></textarea></td>
+                                <td><textarea name="information" ></textarea></td>
                             </tr>
                             <tr>
                                 <td><label>introduction: </label></td>
-                                <td><textarea name="introduction" cols="" rows="" ><?php if(isset($_POST["introduction"])) echo ($_POST["tomtat1"]) ?></textarea></td>
+                                <td><textarea name="introduction"  ></textarea></td>
                             </tr>
                         </table>
                         <div class="button-save">
-                            <button>Save</button>
+                            <input type="submit" name="add" value="Save">
                         </div>
                     </div>
-                </form>    
+                </form>   
     </section> 
